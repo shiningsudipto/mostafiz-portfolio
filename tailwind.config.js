d2afa5a9d6eaea365f5b28d06999f1218c0502f3
shiningsudipto <shiningsudipto@gmail.com>
2023-08-15 01:36:10 +0600

    project setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: '#FF9800',
+        secondaryColor: '#6F34FE',
+        subtitleColor: '#743BFE',
+        titleColor: '#3F396D',
+        textColor: '#8C8797'
+      },
+      fontFamily: {
+        Josefin: 'Josefin Sans',
+        Lato: 'Lato'
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
