0723bb95854473b9be53e85d326f2c72841b2931
shiningsudipto <shiningsudipto@gmail.com>
2023-08-30 10:55:59 +0600

    skill- download btn added

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -11,12 +11,13 @@ const Skills = () => {
                         <Progress className={'text-2xl font-semibold'} progress={90} strokeWidth={6} reduction={0} gradient={[{ stop: 0.0, color: '#00bc9b' }, { stop: 1, color: '#5eaefd' }]} subtitle='Rest API' />
                     </div>
                     <div className='col-span-2 ms-14'>
-                        <div className=''>
+                        <div className='-mt-10'>
                             <h2 className="text-secondaryColor text-2xl font-semibold pt-10">My Skills</h2>
-                            <h3 className="text-titleColor text-5xl font-bold pb-10 pt-4">Beautiful & Unique Digital Experiences</h3>
+                            <h3 className="text-titleColor text-5xl font-bold pb-8 pt-4">Beautiful & Unique Digital Experiences</h3>
                         </div>
                         <p className='text-xl text-textColor'>"Proficient app developer skilled in creating cross-platform applications using Flutter framework. Experienced in integrating Firebase services for real-time databases, authentication, and cloud storage. Well-versed in managing app state and navigation using the GetX package, enhancing user experience. Adept at working with REST APIs to seamlessly connect apps with external data sources, ensuring smooth data retrieval and updates. <br />
                             Passionate about delivering high-quality, responsive, and feature-rich applications that meet both user needs and technical standards."</p>
+                        <button className='primaryBtn mt-4'>Download CV</button>
                     </div>
                 </div>
             </div>
