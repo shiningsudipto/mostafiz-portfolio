327d672c3d3e21186a8074f5859251717c9917d9
shiningsudipto <shiningsudipto@gmail.com>
2023-09-05 16:27:12 +0600

    blog adding form added

---
+++ src/components/BlogAddingModal.jsx
@@ -0,0 +1,95 @@
+import Modal from "./Modal";
+import { useState } from 'react';
+
+const BlogAddingModal = ({ isOpen, setIsOpen }) => {
+    const [formData, setFormData] = useState({
+        title: '',
+        description: '',
+        link: '',
+        imageLink: '', // Add the image link field
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission here
+        console.log('Form Data:', formData);
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <Modal
+            title={"Add A Blog"}
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+        >
+            <div className="bg-gray-100 p-4 flex justify-center items-center">
+                <div className="rounded-lg w-96">
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <label htmlFor="title" className="block text-gray-600 text-sm font-medium mb-2">Title</label>
+                            <input
+                                type="text"
+                                id="title"
+                                name="title"
+                                value={formData.title}
+                                onChange={handleChange}
+                                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                                placeholder="Enter title"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="description" className="block text-gray-600 text-sm font-medium mb-2">Description</label>
+                            <input
+                                type="text"
+                                id="description"
+                                name="description"
+                                value={formData.description}
+                                onChange={handleChange}
+                                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                                placeholder="Enter description"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="link" className="block text-gray-600 text-sm font-medium mb-2">Link</label>
+                            <input
+                                type="text"
+                                id="link"
+                                name="link"
+                                value={formData.link}
+                                onChange={handleChange}
+                                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                                placeholder="Enter link"
+                            />
+                        </div>
+                        {/* Add the image link field */}
+                        <div className="mb-4">
+                            <label htmlFor="imageLink" className="block text-gray-600 text-sm font-medium mb-2">Image Link</label>
+                            <input
+                                type="text"
+                                id="imageLink"
+                                name="imageLink"
+                                value={formData.imageLink}
+                                onChange={handleChange}
+                                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                                placeholder="Enter image link"
+                            />
+                        </div>
+                        <div className="mt-6">
+                            <button type="submit" className="bg-blue-500 text-white font-medium py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </Modal>
+    );
+};
+
+export default BlogAddingModal;
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,40 +1,52 @@
 import { Link } from "react-scroll";
-
+import { useState } from 'react'
+import BlogAddingModal from "./BlogAddingModal";
 const NavigationBar = () => {
+    let [isOpen, setIsOpen] = useState(false)
 
     const navLinks = <>
-        <li> <Link href="#home">Home</Link> </li>
-        <li> <Link href="#services">Services</Link> </li>
-        <li> <Link href="#about">About</Link> </li>
-        <li> <Link href="#portfolio">Portfolio</Link> </li>
-        <li> <Link href="#blog">Blog</Link> </li>
+        <li> <Link to="#home">Home</Link> </li>
+        <li onClick={() => setIsOpen(!isOpen)}>
+            <p>Add Blog</p>
+        </li>
+        <li> <Link to="#services">Services</Link> </li>
+        <li> <Link to="#about">About</Link> </li>
+        <li> <Link to="#portfolio">Portfolio</Link> </li>
+        <li> <Link to="#blog">Blog</Link> </li>
     </>
 
     return (
-        <div className="">
-            <div className="navbar pt-5">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            {navLinks}
-                        </ul>
+        <>
+            <BlogAddingModal
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+            ></BlogAddingModal>
+            <div className="">
+                <div className="navbar pt-5">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </label>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                {navLinks}
+                            </ul>
+                        </div>
+                        <a className="normal-case text-xl">Fiz</a>
                     </div>
-                    <a className="normal-case text-xl">Fiz</a>
-                </div>
 
-                <div className="navbar-end">
-                    <div className="navbar-center hidden lg:flex">
-                        <ul className="menu menu-horizontal text-lg font-semibold text-titleColor px-1 font-Josefin">
-                            {navLinks}
-                        </ul>
+                    <div className="navbar-end">
+                        <div className="navbar-center hidden lg:flex">
+                            <ul className="menu menu-horizontal text-lg font-semibold text-titleColor px-1 font-Josefin">
+                                {navLinks}
+                            </ul>
+                        </div>
+                        <button className="primaryBtn ms-2">Contact</button>
                     </div>
-                    <button className="primaryBtn ms-2">Contact</button>
                 </div>
             </div>
-        </div>
+        </>
+
     );
 };
 
